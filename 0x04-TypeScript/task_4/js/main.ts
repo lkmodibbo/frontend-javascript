1456be553c2afe4ebd28135f4f8d90dd909f8fbc
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-09 22:42:38 +0100

    corredted

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,31 +4,26 @@
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
 
-// Export constants for subjects
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const cpp = new Cpp();
+const java = new Java();
+const react = new React();
 
-// Export teacher object
-export const cTeacher: Subjects.Teacher = {
+const cTeacher: Subjects.Teacher = {
   firstName: "John",
   lastName: "Doe",
   experienceTeachingC: 10
 };
 
-// Cpp
 console.log("C++");
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-// Java
 console.log("Java");
 java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-// React
 console.log("React");
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
